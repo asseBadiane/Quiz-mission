0262ac41366e7d94eaa60e813814799dcf9ab275
asseBadiane <badianeasse99@gmail.com>
2023-05-24 11:11:39 +0000

    Importation fichier json et poser première question

--- questionnaire.py
+++ questionnaire.py
@@ -1,23 +1,4 @@
-# PROJET QUESTIONNAIRE V3 : POO
-#
-# - Pratiquer sur la POO
-# - Travailler sur du code existant
-# - Mener un raisonnement
-#
-# -> Définir les entitées (données, actions)
-#
-# Question
-#    - titre       - str
-#    - choix       - (str)
-#    - bonne_reponse   - str
-#
-#    - poser()  -> bool
-#
-# Questionnaire
-#    - questions      - (Question)
-#
-#    - lancer()
-#
+import json
 
 class Question:
     def __init__(self, titre, choix, bonne_reponse):
@@ -25,9 +6,12 @@ class Question:
         self.choix = choix
         self.bonne_reponse = bonne_reponse
 
-    def FromData(data):
-        # ....
-        q = Question(data[2], data[0], data[1])
+    def FromJsonData(data):
+        titre = data['titre']
+        choix = [i[0] for i in data['choix']]
+        bonne_reponse = [i[0] for i in data['choix'] if i[1]] 
+        q = Question(titre, choix, bonne_reponse[0])
+
         return q
 
     def poser(self):
@@ -88,12 +72,23 @@ lancer_questionnaire(questionnaire)"""
 # q = Question.FromData(data)
 # print(q.__dict__)
 
-Questionnaire(
-    (
-    Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris"), 
-    Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome"),
-    Question("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles")
-    )
-).lancer()
+# Questionnaire(
+#     (
+#     Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris"), 
+#     Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome"),
+#     Question("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles")
+#     )
+# ).lancer()
 
 
+fileName = "animaux_leschats_debutant.json"
+file = open(fileName, "r")
+
+jsonData = file.read()
+file.close()
+
+questionnaire_data_json = json.loads(jsonData)
+questionnaire_data = questionnaire_data_json['questions']
+# print(questionnaire_data[0])
+q = Question.FromJsonData(questionnaire_data[0])
+q.poser()
